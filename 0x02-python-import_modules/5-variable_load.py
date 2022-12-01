62d8ace87b5d2d2c1beb864df4458858de43eb55
Don-Java <donijilad@gmail.com>
2022-12-01 12:39:42 -0800

    solution

--- 0x02-python-import_modules/5-variable_load.py
+++ 0x02-python-import_modules/5-variable_load.py
@@ -1,8 +1,6 @@
-5-variable_load.py
 #!/usr/bin/python3
 
 if __name__ == "__main__":
-    """Print the value of variable a from variable_load_5."""
     from variable_load_5 import a
 
     print(a)
