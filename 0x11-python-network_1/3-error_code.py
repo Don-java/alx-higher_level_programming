61a81fc66e5987dc9183470753cba89812862aa4
Don-Java <donijilad@gmail.com>
2023-03-31 11:10:41 -0700

    correction of 3

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -1,18 +1,13 @@
 #!/usr/bin/python3
-"""Send request"""
-import urllib.request
-import urllib.error
-import sys
+"""Python script that takes in a URL, sends a request to the URL
+and displays the body of the response (decoded in utf-8)."""
 
-
-def sender():
-    """sender"""
-    try:
-        with urllib.request.urlopen(sys.argv[1]) as response:
-            html = response.read()
-            print(html.decode("utf-8"))
-    except urllib.error.HTTPError as e:
-        print("Error code: {}".format(e.code))
+from urllib import request, error
+from sys import argv
 
 if __name__ == "__main__":
-    sender()
+        try:
+            with request.urlopen(argv[1]) as page:
+                print(page.read().decode('utf-8'))
+        except error.HTTPError as e:
+            print("Error code: {}".format(e.code))
