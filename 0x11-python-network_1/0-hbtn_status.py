995ecd48781b30b9b06079741528a1381a28e764
Don-Java <donijilad@gmail.com>
2023-03-31 10:47:31 -0700

    Python - Network #1

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
-"""my body header"""
-if __name__ == "__main__":
-    import urllib.request
+"""fetches https://intranet.hbtn.io/status"""
+import urllib.request as request
 
-    with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
-        print("Body response:")
-        ty = response.read()
-        print("\t- type: {}".format(type(response.read())))
-        print("\t- content: {}".format(ty))
-        print("\t- utf8 content: {}".format(ty.decode('utf-8')))
+
+if __name__ == "__main__":
+    with request.urlopen('https://intranet.hbtn.io/status') as r:
+        html = r.read()
+        print('Body response:')
+        print("\t- type: {}".format(type(html)))
+        print("\t- content: {}".format(html))
+        print("\t- utf8 content: {}".format(html.decode('utf-8')))
